51e17d78fa21c74d715008e4d2ba75dc1e90ca83
marcobrasci1986 <marco_brasci@hotmail.com>
2018-02-10 10:14:41 +0100

    Initial commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './heroes/hero';
+import { HEROES } from './heroes/mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes (): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  constructor (private messageService: MessageService) {
+  }
+
+}
